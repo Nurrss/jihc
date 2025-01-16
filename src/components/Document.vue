7df8dc66c1b4b72844de4865d0175b1a692d34f1
Nursultan <nurrs.serkul@gmail.com>
2025-01-16 12:08:37 +0500

    updated document link

--- src/components/Document.vue
+++ src/components/Document.vue
@@ -4,7 +4,7 @@
       <a
         class="p-0 m-0 link-item"
         href="https://drive.google.com/file/d/1O1eSO0YejLQcWk36qJ7gNIpB2TXJo8Hk/view?usp=sharing"
-        >
+      >
         {{ $t("DocumentP1") }}
       </a>
     </div>
@@ -14,7 +14,7 @@
         href="https://drive.google.com/file/d/19kfq8NnqcerWO1lGxhS2S9rchQlYeTDB/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP2") }}
+        {{ $t("DocumentP2") }}
       </a>
     </div>
 
@@ -23,7 +23,7 @@
         href="https://drive.google.com/file/d/1MWtj9-I35LWURHw0irLza3FIkAT6hYuv/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP3") }}
+        {{ $t("DocumentP3") }}
       </a>
     </div>
     <div class="link-container mb-3">
@@ -31,7 +31,7 @@
         href="https://drive.google.com/file/d/1o0RYjM-MnjPPcxG5PUSlGSsuY7ET-jMk/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP4") }}
+        {{ $t("DocumentP4") }}
       </a>
     </div>
 
@@ -40,7 +40,7 @@
         href="https://drive.google.com/file/d/1FuUuQ5lVHQpA_Gsl2g4-7raorYnm2Ziz/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP5") }}
+        {{ $t("DocumentP5") }}
       </a>
     </div>
     <div class="link-container mb-3">
@@ -48,7 +48,7 @@
         href="https://drive.google.com/file/d/14MqalZjhuVWRRL114OJnA60rvIVlU3Yv/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP6") }}
+        {{ $t("DocumentP6") }}
       </a>
     </div>
     <div class="link-container mb-3">
@@ -56,15 +56,15 @@
         href="https://drive.google.com/file/d/14MqalZjhuVWRRL114OJnA60rvIVlU3Yv/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP7") }}
-    </a>
+        {{ $t("DocumentP7") }}
+      </a>
     </div>
     <div class="link-container mb-3">
       <a
-        href="https://drive.google.com/file/d/1sb4l5c_Ayw2IPIK4P51YGr_24mhnn6Xg/view?usp=sharing"
+        href="https://docs.google.com/document/d/1_POVHmkCqyv072Fcuf1oqLQ08d0ot5Gl/edit?usp=sharing&ouid=106749311590716813947&rtpof=true&sd=true"
         class="link-item"
       >
-      {{ $t("DocumentP8") }}
+        {{ $t("DocumentP8") }}
       </a>
     </div>
     <div class="link-container mb-3">
@@ -75,26 +75,27 @@
         href=" https://drive.google.com/file/d/14OtLIZdEZGAQ60Mx89vfnMPh_YePCKs1/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP10") }}
+        {{ $t("DocumentP10") }}
       </a>
       <a
         href="https://drive.google.com/file/d/1zlgOuP-xXgcsDwJ2KWQJgoTRXx8FckPt/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP11") }}
+        {{ $t("DocumentP11") }}
       </a>
       <a
         href="https://drive.google.com/file/d/1X08f0MgkGzQFyMLIOGz6HIkU3Cud9vX-/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP12") }}</a>
+        {{ $t("DocumentP12") }}</a
+      >
       <a
         href="https://drive.google.com/file/d/1dEjQX0PxP3LbtvE3-MpObo2Z7Y_ZLjwA/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP13") }}
+        {{ $t("DocumentP13") }}
       </a>
-       <a
+      <a
         href="https://drive.google.com/file/d/1YrjURjAh8k6a6GXMA3XnrgMhbr96J-p7/view?usp=sharing"
         class="link-item"
       >
@@ -161,7 +162,7 @@
         href="https://drive.google.com/file/d/1tpbimZU4h1OCVcRlVQXd5ctmDe6KK_JS/view?usp=sharing"
         class="link-item"
       >
-      {{ $t("DocumentP14") }}
+        {{ $t("DocumentP14") }}
       </a>
     </div>
   </div>
